deca60e708eca3f12faa36c219fe5edf906ecc38
maikolcw <whysoraccoony@gmail.com>
2023-02-24 01:16:37 -0800

    updated documentation and added counts to csv file

--- src/GrabWikiLinks.java
+++ src/GrabWikiLinks.java
@@ -17,10 +17,17 @@ import java.util.HashMap;
  * link and throws an error if the link is not a valid Wikipedia link(check isProperWikiLink method for more details).
  * The program will not crash, but instead show the error message and prompt the user for a proper Wikipedia link again.
  * This program gracefully accepts a valid integer, n, between one and twenty. The program will then scrape the link
- * provided of all Wikipedia links embedded in the page and store them in a HashMap that has the link as a key and an
- * integer as the value for number of occurrences. The program will then repeat the steps of scraping Wikipedia links
- * for all the newly found links and store them in the HashMap for n times. The code is optimized to not visit any links
- * already visited and the results are printed to console and a CSV file.
+ * provided of all Wikipedia links embedded in the page, filter for proper Wikipedia links, and store them in a HashMap
+ * called currentLiftOfFoundLinks that has the link as a key and an integer as the value for number of occurrences of
+ * that link. The program will then repeat the steps of scraping Wikipedia links for all the newly found links. If the
+ * link has been visited (is in the HashMap listOfLinksVisited) its counter will be incremented. If not, the link will
+ * be visited and scraped then added to listOfLinksVisited. Each time this is done the newly scraped links are put into
+ * a HashMap called nextListOfFoundLinks where they will be stored until all links of the currentListOfFoundLinks are
+ * cycled through. Once the currentListOfFoundLinks are exhausted it is cleared and the links from the
+ * nextListOfFoundLinks are copied to the currentListOfFoundLinks. This process is repeated n times. Once the program
+ * has completely cycled through n rotations, the data stored in listOfLinksVisited will be used to show total number of
+ * link occurrences, unique number of links, and the address of the links along with their respective occurrences. The
+ * data is converted into a csv file.
  */
 public class GrabWikiLinks {
 
@@ -124,11 +131,16 @@ public class GrabWikiLinks {
     private static void convertHashMapToCSVFile() {
         System.out.println("Converting HashMap to csv file");
         File csvFile = new File("WikipediaLinks.csv");
+        int totalCount = 0;
+        int uniqueCount = listOfLinksVisited.size();
         try {
             PrintWriter out = new PrintWriter(csvFile);
             for (Map.Entry<String,Integer> entry : listOfLinksVisited.entrySet()) {
                 out.printf("%s, %d\n", entry.getKey(), entry.getValue());
+                totalCount = totalCount + entry.getValue();
             }
+            out.printf("%s, %d\n", "Total counts", totalCount);
+            out.printf("%s, %d\n", "Unique counts", uniqueCount);
             out.close();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
